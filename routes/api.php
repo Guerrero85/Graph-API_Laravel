4e4043decd7286b7fc26f55fd86e2e71d6732c8f
Cesar Guerrero <34452566+Guerrero85@users.noreply.github.com>
2022-10-23 21:52:22 -0400

    cambios

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use PharIo\Manifest\Url;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/test', function() {
+    return "Hola Mundo";
 });
